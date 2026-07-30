7a5f647203ca3afaa678be0fcd5b95559568e0d9
Yash Jawale <yash.jawale@rtcamp.com>
2026-07-30 19:58:40 +0530

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,38 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Copyright (C) 2017 Red Hat, Inc.
+ */
+
+import React, { useEffect, useState } from 'react';
+import { Alert } from "@patternfly/react-core/dist/esm/components/Alert/index.js";
+import { Card, CardBody, CardTitle } from "@patternfly/react-core/dist/esm/components/Card/index.js";
+import { Page } from '@patternfly/react-core/dist/esm/components/Page/index.js';
+
+import cockpit from 'cockpit';
+
+const _ = cockpit.gettext;
+
+export const Application = () => {
+    const [hostname, setHostname] = useState(_("Unknown"));
+
+    useEffect(() => {
+        const hostname = cockpit.file('/etc/hostname');
+        hostname.watch(content => setHostname(content?.trim() ?? ""));
+        return hostname.close;
+    }, []);
+
+    return (
+        <Page className='pf-m-no-sidebar'>
+            <Card>
+                <CardTitle>Starter Kit</CardTitle>
+                <CardBody>
+                    <Alert
+                    variant="info"
+                    title={ cockpit.format(_("Running on $0"), hostname) }
+                    />
+                </CardBody>
+            </Card>
+        </Page>
+    );
+};
